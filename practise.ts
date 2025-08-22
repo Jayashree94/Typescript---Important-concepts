a367d4731a8cae3c7e55eda260c3b7504cbe6dc8
Jayashree <s.jayashree94@gmail.com>
2025-08-23 05:07:24 +0530

    typesafety

--- practise.ts
+++ practise.ts
@@ -1,6 +1,6 @@
 let firstName:String = "Jayashree;"
 let employeeNo:Number = 101120;
-let isMajor = false;
+let isMajor:Boolean = false;
 
 let subjects: string[] = ["English", "Tamil", "Maths"];
 let employee:{
@@ -12,5 +12,11 @@ phnumber:988454354,
 address: "Chennai",
 pincode:890890
 }
+
+let employeeDetails : {employeeName:String; employeeNumber:number; employeeDesignamtion: String}[] = [
+    {employeeName:"jaya", employeeNumber:454353454, employeeDesignamtion:"SE"},
+    {employeeName:"ruba", employeeNumber:4535355, employeeDesignamtion:"SRE"},
+    {employeeName:"heloo", employeeNumber:5434534534, employeeDesignamtion:"RRR"}
+]
 
 
