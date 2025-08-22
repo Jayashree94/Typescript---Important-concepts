e51553d7d4ded54a84226bf02554e01688305445
Jayashree <s.jayashree94@gmail.com>
2025-08-23 04:58:29 +0530

    typesafety

---
+++ practise.ts
@@ -0,0 +1,16 @@
+let firstName:String = "Jayashree;"
+let employeeNo:Number = 101120;
+let isMajor = false;
+
+let subjects: string[] = ["English", "Tamil", "Maths"];
+let employee:{
+    phnumber:number;
+    address:string;
+    pincode:number
+}={
+phnumber:988454354,
+address: "Chennai",
+pincode:890890
+}
+
+
