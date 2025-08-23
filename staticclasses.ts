d9b1a4b1165bca42523652af24f06f0295c6e6cf
Jayashree <s.jayashree94@gmail.com>
2025-08-24 05:24:48 +0530

    ts classes

--- staticclasses.ts
+++ staticclasses.ts
@@ -1,18 +1,21 @@
-//public - access anywhere
-//private - access only inside class
-//protected - access inside and derived classes
+//static - access with the help of class only
+//global 
 
-class Employee{
-    readonly _name: string;
+class Employee1{
+    //static readonly _name: string;
+    static _name : string;
     _age:number;
 
     constructor(name: string, age: number){
-        this._name = name;
+    //    this._name = name;
+    Employee1._name = name;
         this._age = age;
     }
     
     get name(){
-        return this._name;
+    //    return this._name;
+
+    return Employee1._name;
     }
 
     //set name(value:string){                 -> cannot assign value since it is a readonly property
@@ -35,4 +38,4 @@ class Employee{
 }
 
 
-const emp:Employee = new Employee("Jays", 30);
+const emp1:Employee1 = new Employee1("Jays", 30);
